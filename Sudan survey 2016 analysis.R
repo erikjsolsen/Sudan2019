0d412850f7f34d500013a532e03d64ad24c15718
Erik Olsen <eriko@imr.no>
2016-10-28 09:00:53 +0300

    New script for Sudan 2016
    New survey 2016 analysis script.
    Routine for mapping reef areas

---
+++ Sudan survey 2016 analysis.R
@@ -0,0 +1,103 @@
+#' @title Analysis of Oct - Dec 2016 Sudan Survey
+#' @description mapping, analysis of data to pick out errors etc
+#' @description updated with data files for entire survey
+#' 
+
+#' LIBRARIES AND SOURCES
+#' ------------------------
+library("maps")
+library("mapdata")
+library("ggplot2")
+library("RColorBrewer")
+
+
+#' IMPORTING DATA
+#' ------------------------
+setwd("~/ownCloud/Research/Sudan/2016/Survey OctDec2016/survey data")
+#bruv<-read.csv2("BRUV UVC/BRUV UVC stations FINAL.csv", dec=",") 
+stations<-read.delim("Hl and TRAPS/red sea 2016002 station export.txt", )
+biology<-read.delim("Hl and TRAPS/red sea 2016002 data export.txt")
+#ctd<-read.csv2("CTD/CTD2015.csv")
+
+#' selecting columns for stations and biology & adding positions to biology
+bc<-c(2,3,4,5,6,8,10,11,12,13,14,15,18,20,21,23)
+sc<-c(2,3,5,7,10,11,12,13,14,15,16, 17, 18)
+bc<-biology[,bc]
+sc<-stations[,sc]
+bc$Station<-bc$SurvStat
+colnames(bc)[9]<-c("weight")
+colnames(bc)[6]<-c("duration")
+colnames(bc)[8]<-c("length")
+colnames(bc)[11]<-c("gonads")
+
+sc$Station.Id<-sc$Stat_Unique
+colnames(sc)[1]<-c("Station")
+colnames(sc)[6]<-c("depth")
+colnames(sc)[7]<-c("date_start")
+colnames(sc)[9]<-c("time_start")
+colnames(sc)[10]<-c("time_stop")
+colnames(sc)[13]<-c("date_stop")
+
+
+#' combining stations file and BRUV + UVC for a complete station list. 
+#select same columns from BRUV and stations
+#give same names
+#colnames(bruv)<-colnames(stations)
+#AllGear<-rbind(bc, sc)
+
+ps<-data.frame(x=37.21709967,y=19.600512, group=1, survey=2012901 )
+
+
+#' CORRECT NAMES and POSITIONS according to STAIONS
+#' 
+
+bc$lat<-c(1)
+bc$lon<-c(1)
+bc$name<-c("name")
+
+for (i in 1:length(bc$lat)) 
+{
+  bc$lat[i]<- subset(sc, Station==bc$Station[i])[,3]
+  bc$lon[i]<- subset(sc, Station==bc$Station[i])[,4]
+  bc$name[i]<- as.character(subset(sc, Station==bc$Station[i])[,2])
+}
+
+write.csv2(biology, "survey_2016002_biology_corr_name_pos_effort_soak.csv")
+
+#' MAPPING ALL DATA
+#' --------------------
+world<- map_data("worldHires", c("Sudan", "Egypt", "Ethiopia")) 
+
+stationmap<-ggplot(world, aes(x=long, y=lat, group=group)) + geom_polygon(colour="gray35", fill="gray85") + geom_point(data=sc, shape="+", size=6, aes(x=Longitude, y=Latitude, group=Survey, colour= Gear))  +  coord_cartesian(xlim = c(36.6, 38.9), ylim=c(17.7, 22.5)) + theme_classic() 
+stationmap 
+
+
+
+#'adding place names
+stationnames<-read.csv2("stations place names.csv")
+stationmap <- stationmap + geom_point(data=stationnames, shape="#", size=6, colour="red2", aes(x=LON, y=LAT, group=Area.No)) + geom_text(data=stationnames, label=stationnames$AreaName, size=4,  hjust=1.15,  aes(x=LON, y=LAT, group=Area.No)) 
+stationmap
+
+#' reefmap
+reefmap <- ggplot(world, aes(x=long, y=lat, group=group)) + geom_polygon(colour="gray35", fill="gray85") + geom_point(data=stationnames, size=3, aes(x=LON, y=LAT, group=Area.No, colour=as.factor(Part))) + geom_text(data=stationnames, label=stationnames$AreaName, size=2,  hjust=1.15,  aes(x=LON, y=LAT, group=Area.No)) + theme_classic() + scale_color_brewer(palette = "Set1", name="Survey parts") + scale_shape_discrete()
+
+#' reefmap whole sudan
+reefmap +  coord_cartesian(xlim = c(36, 38.9), ylim=c(17.7, 22.5)) 
+ggsave("reefmap_all.png", width=4.3, height=6)
+
+#' southern area only
+reefmap +  coord_cartesian(xlim = c(36.6, 38.9), ylim=c(17.7, 19.5))
+ggsave("reefmap_south.png", width=5, height=5)
+
+
+#' PANELLED MAP BY GEAR
+#' --------------------------
+survey16map<-ggplot(sc, aes(x=Longitude, y=Latitude, group=Gear)) + geom_point(colour = "blue", shape="+", size=6)  + facet_wrap(~Gear)   + geom_polygon(data=world, colour="gray35", fill="gray85", aes(x=long, y=lat, group=group))  + geom_text(data=ps, label="PZU", size=4,  hjust=1.15,  aes(x=x, y=y, group=group)) +  coord_cartesian(xlim = c(36.8, 38.9), ylim=c(17.7, 22.5)) + theme_classic() 
+survey16map
+#' 
+
+#' L-W plots
+lwplot <- ggplot(bc, aes(x=length, y=weight, group=Species)) + geom_point(colour="steelblue") + facet_wrap(~Species, scale ="free")
+lwplot
+
+# some errors need to be corrected
